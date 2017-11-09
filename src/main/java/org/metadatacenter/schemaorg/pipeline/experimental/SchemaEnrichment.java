b4fb88172b08fd73ac9aa7a4d1ad49f8fbd31ddf
johardi <josef.hardi@stanford.edu>
2017-11-09 15:39:48 -0800

    Use schema:additionalType to hint the namespace rather that from user's input

--- src/main/java/org/metadatacenter/schemaorg/pipeline/experimental/SchemaEnrichment.java
+++ src/main/java/org/metadatacenter/schemaorg/pipeline/experimental/SchemaEnrichment.java
@@ -1,9 +1,11 @@
 package org.metadatacenter.schemaorg.pipeline.experimental;
 
+import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import com.google.common.collect.Lists;
 
 public class SchemaEnrichment {
 
@@ -13,6 +15,8 @@ public class SchemaEnrichment {
   private static final String SCHEMA_NAME_SHORT = "name";
   private static final String SCHEMA_IDENTIFIER = "schema:identifier";
   private static final String SCHEMA_IDENTIFIER_SHORT = "identifier";
+  private static final String SCHEMA_ADDITIONAL_TYPE = "schema:additionalType";
+  private static final String SCHEMA_ADDITIONAL_TYPE_SHORT = "additionalType";
 
   public static String fillOutIdFromObjectName(String jsonString, NameLookup lookup) {
     final JSONObject jsonObject = new JSONObject(jsonString);
@@ -62,45 +66,63 @@ public class SchemaEnrichment {
     }
   }
 
-  public static String fillOutIdFromObjectIdentifier(String jsonString, String namespace, IdResolver resolver) {
+  public static String fillOutIdFromObjectIdentifier(String jsonString, IdResolver resolver) {
     final JSONObject jsonObject = new JSONObject(jsonString);
-    doFillOut(jsonObject, namespace, resolver);
+    doFillOut(jsonObject, resolver);
     return jsonObject.toString();
   }
 
-  private static void doFillOut(JSONObject jsonObject, String namespace, IdResolver resolver) {
-    fillOutId(jsonObject, namespace, resolver);
+  private static void doFillOut(JSONObject jsonObject, IdResolver resolver) {
+    fillOutId(jsonObject, resolver);
     for (String key : jsonObject.keySet()) {
       Object obj = jsonObject.get(key);
       if (obj instanceof JSONObject) {
-        doFillOut((JSONObject) obj, namespace, resolver);
+        doFillOut((JSONObject) obj, resolver);
+      } else if (obj instanceof JSONArray) {
+        JSONArray jsonArray = (JSONArray) obj;
+        for (Object arrayItem : jsonArray) {
+          if (arrayItem instanceof JSONObject) {
+            doFillOut((JSONObject) arrayItem, resolver);
+          }
+        }
       }
     }
   }
 
-  private static void fillOutId(JSONObject jsonObject, String namespace, IdResolver resolver) {
-    if (!hasId(jsonObject) && hasSchemaIdentifier(jsonObject)) {
+  private static void fillOutId(JSONObject jsonObject, IdResolver resolver) {
+    if (!hasId(jsonObject) && hasSchemaIdentifier(jsonObject) && hasSchemaAdditionalType(jsonObject)) {
       Object obj = getSchemaIdentifier(jsonObject);
+      List<String> candidateNamespaces = getSchemaAdditionalType(jsonObject);
       if (obj instanceof String) {
-        String codedId = (String) obj;
-        Optional<String> id = resolver.resolve(codedId, namespace);
-        if (id.isPresent()) {
-          jsonObject.put(JSONLD_ID, id.get());
+        String shortId = (String) obj;
+        String id = resolveShortId(shortId, candidateNamespaces, resolver);
+        if (id != null) {
+          jsonObject.put(JSONLD_ID, id);
         }
       } else if (obj instanceof JSONArray) {
-        JSONArray codedIdArray = (JSONArray) obj;
-        for (Object codedIdObject : codedIdArray) {
-          if (codedIdObject instanceof String) {
-            String codedId = (String) codedIdObject;
-            Optional<String> id = resolver.resolve(codedId, namespace);
-            if (id.isPresent()) {
-              jsonObject.put(JSONLD_ID, id.get());
-              break;
+        for (Object candidateIdentifier : (JSONArray) obj) {
+          if (candidateIdentifier instanceof String) {
+            String shortId = (String) candidateIdentifier;
+            String id = resolveShortId(shortId, candidateNamespaces, resolver);
+            if (id != null) {
+              jsonObject.put(JSONLD_ID, id);
+              break; // found the best resolved short identifiers
             }
           }
         }
       }
     }
+  }
+
+  @Nullable
+  private static String resolveShortId(String shortId, List<String> candidateNamespaces, IdResolver resolver) {
+    for (String namespace : candidateNamespaces) {
+      Optional<String> resolvedId = resolver.resolve(shortId, namespace);
+      if (resolvedId.isPresent()) {
+        return resolvedId.get();
+      }
+    }
+    return null;
   }
 
   private static boolean hasId(JSONObject jsonObject) {
@@ -113,6 +135,10 @@ public class SchemaEnrichment {
 
   private static boolean hasSchemaIdentifier(JSONObject jsonObject) {
     return jsonObject.has(SCHEMA_IDENTIFIER) || jsonObject.has(SCHEMA_IDENTIFIER_SHORT);
+  }
+
+  private static boolean hasSchemaAdditionalType(JSONObject jsonObject) {
+    return jsonObject.has(SCHEMA_ADDITIONAL_TYPE) || jsonObject.has(SCHEMA_ADDITIONAL_TYPE_SHORT);
   }
 
   @Nullable
@@ -135,5 +161,27 @@ public class SchemaEnrichment {
       obj = jsonObject.get(SCHEMA_IDENTIFIER_SHORT);
     }
     return obj;
+  }
+
+  private static List<String> getSchemaAdditionalType(JSONObject jsonObject) {
+    List<String> types = Lists.newArrayList();
+    if (jsonObject.has(SCHEMA_ADDITIONAL_TYPE)) {
+      Object obj = jsonObject.get(SCHEMA_ADDITIONAL_TYPE);
+      collectTypes(obj, types);
+    } else if (jsonObject.has(SCHEMA_ADDITIONAL_TYPE_SHORT)) {
+      Object obj = jsonObject.get(SCHEMA_ADDITIONAL_TYPE_SHORT);
+      collectTypes(obj, types);
+    }
+    return types;
+  }
+
+  private static void collectTypes(Object object, List<String> types) {
+    if (object instanceof String) {
+      types.add((String) object);
+    } else if (object instanceof JSONArray) {
+      for (Object arrayItem : (JSONArray) object) {
+        collectTypes(arrayItem, types);
+      }
+    }
   }
 }
