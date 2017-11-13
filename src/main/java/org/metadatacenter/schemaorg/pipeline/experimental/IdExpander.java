74c6d3b51a5962e3d9c819acf52225ac0dd2ada6
johardi <josef.hardi@stanford.edu>
2017-11-13 10:27:58 -0800

    Support additional parameters in the id expander

--- src/main/java/org/metadatacenter/schemaorg/pipeline/experimental/IdExpander.java
+++ src/main/java/org/metadatacenter/schemaorg/pipeline/experimental/IdExpander.java
@@ -1,8 +1,11 @@
 package org.metadatacenter.schemaorg.pipeline.experimental;
 
 import java.util.Optional;
+import java.util.Properties;
 
 public interface IdExpander {
 
   Optional<String> expand(String id, String namespace);
+
+  Optional<String> expand(String id, String namespace, Properties additionalParameters);
 }
