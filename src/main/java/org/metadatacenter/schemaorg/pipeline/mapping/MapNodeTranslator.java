b4b0b09b7906796e9fab7729071e8cdaa6e7f497
johardi <josef.hardi@stanford.edu>
2017-11-22 11:30:46 -0800

    Add an extra parameter to select the mapping language

--- src/main/java/org/metadatacenter/schemaorg/pipeline/mapping/MapNodeTranslator.java
+++ src/main/java/org/metadatacenter/schemaorg/pipeline/mapping/MapNodeTranslator.java
@@ -2,12 +2,25 @@ package org.metadatacenter.schemaorg.pipeline.mapping;
 
 import org.metadatacenter.schemaorg.pipeline.caml.databind.AttributeMapper;
 import org.metadatacenter.schemaorg.pipeline.mapmodel.ObjectNode;
+import org.metadatacenter.schemaorg.pipeline.rml.databind.RmlMapper;
 
 public class MapNodeTranslator {
 
-  public static String translate(TranslatorHandler handler, final String mappingString) {
-    final AttributeMapper mapper = new AttributeMapper();
-    ObjectNode objectNode = mapper.readText(mappingString);
+  public static String translate(TranslatorHandler handler, String mappingString) {
+    return translate(handler, mappingString, MappingLanguages.CAML);
+  }
+
+  public static String translate(TranslatorHandler handler, String mappingString, String language) {
+    ObjectNode objectNode = null;
+    if (MappingLanguages.CAML.equals(language)) {
+      final AttributeMapper mapper = new AttributeMapper();
+      objectNode = mapper.readText(mappingString);
+    } else if (MappingLanguages.RML.equals(language)) {
+      final RmlMapper mapper = new RmlMapper();
+      objectNode = mapper.readText(mappingString);
+    } else {
+      throw new IllegalArgumentException("The language selection '" + language + "' is not supported yet");
+    }
     return translate(handler, objectNode);
   }
 
---
+++ src/main/java/org/metadatacenter/schemaorg/pipeline/mapping/MappingLanguages.java
@@ -0,0 +1,7 @@
+package org.metadatacenter.schemaorg.pipeline.mapping;
+
+public final class MappingLanguages {
+
+  public static final String CAML = "caml";
+  public static final String RML = "rml";
+}
